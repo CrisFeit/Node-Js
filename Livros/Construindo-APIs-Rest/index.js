d9207872dbb76a510dc107936a590fc422b5659f
CrisFeit <cristiano.feitosa@corebiz.com.br>
2020-03-13 16:01:28 -0300

    livros/ apis/ - separando libs

--- Livros/Construindo-APIs-Rest/index.js
+++ Livros/Construindo-APIs-Rest/index.js
@@ -1,16 +1,10 @@
 const express        = require('express');
 const consign        = require('consign')
 const app            = express();
-const PORT           = 3000;
-
-app.set("json spaces" , 4)
 
 consign()
 .include('models')
+.then('libs/middlewares.js')
 .then('routes')
+.then('libs/boot.js')
 .into(app)
-
-
-app.listen(PORT , ()=> {
-  console.log(`NTask API - porta ${PORT}`);
-});
