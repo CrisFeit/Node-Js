8be176ee727190e37942a06311f31fc29c384f37
CrisFeit <cristiano.feitosa@corebiz.com.br>
2020-03-15 20:46:44 -0300

    TM add: Pug template engine

--- Livros/Construindo-APIs-Rest/index.js
+++ Livros/Construindo-APIs-Rest/index.js
@@ -3,7 +3,8 @@ const consign        = require('consign')
 const app            = express();
 
 consign()
-.include('models')
+.include('db.js')
+.then('models')
 .then('libs/middlewares.js')
 .then('routes')
 .then('libs/boot.js')
