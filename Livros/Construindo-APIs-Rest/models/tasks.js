77f2acf46e438d61fd5354e5e095a3506efa49a6
CrisFeit <cristiano.feitosa@corebiz.com.br>
2020-03-29 21:22:47 -0300

    Backend Oministack 11

--- Livros/Construindo-APIs-Rest/models/tasks.js
+++ Livros/Construindo-APIs-Rest/models/tasks.js
@@ -1,3 +1,33 @@
+module.exports = (sequelize, DataType) => {
+  const Tasks = sequelize.define('Tasks', {
+    id : {
+      type: DataType.INTEGER,
+      primaryKey : true,
+      autoIcrement: true,
+    },
+    title : {
+      type: DataType.STRING,
+      allowNull : false,
+      validate : {
+        notEmpty : true
+      }
+    },
+    done : {
+      type: DataType.BOOLEAN,
+      allowNull : false,
+      defaultValue :false
+    }
+  },{
+    classMethods : {
+      associate: (models) => {
+        Tasks.belongsTo(models.Users)
+      }
+    }
+  })
+  return Tasks
+}
+
+
 module.exports =  app  => {
   return {
     findAll : (params,callback) => {
--- RocketSeat/Modulo-1/index.js
+++ RocketSeat/Modulo-1/index.js
@@ -8,26 +8,45 @@ server.use(express.json());
 
 const users = ['Diego','Claudio','Victor'];
 
+server.use((req,res,next)=>{
+  console.log(`Metodo : ${req.method} \nURL:${req.url} \nParams:${JSON.stringify(req.params)}`);
+
+  return next();
+})
+
+function checkUserExists(req,res,next){
+  if(!req.body.name){
+    return res.status(400).json({error: 'User name is required'})
+  }
+  return next()
+}
+
+function checkUserInArray(req,res,next){
+  const user = user[req.params.index]
+  if (!users[req.params.index]){
+    return res.status(400).json( { error : 'User does not exists' } )
+  }
+  req.user = user; // create a atribute in req
+  return next()
+}
 server.get('/users', (req ,res) => {
     return res.json(users);
 })
 
-server.get('/users/:index' , (req,res) => {
-  const nome = req.query.nome; // Query Params ?teste=1
-  const id   = req.params.id; // Query Params ?teste=1
-  const { index } = req.params
-  console.log(index)
+server.get('/users/:index' ,checkUserInArray, (req,res) => {
+  // const nome = req.query.nome; // Query Params ?teste=1
+  // const id   = req.params.id; // Query Params ?teste=1
   // return res.json({message : `Hello ${nome}`});
-  return res.json({message : `Buscando usuario ${users[index]}`});
+  return res.json(req.user);
 })
 
-server.post('/users', (req,res) => {
+server.post('/users', checkUserExists,(req,res) => {
   const { name } = req.body
   users.push(name);
   return res.json(users);
 })
 
-server.put('/users/:index', (req,res) => {
+server.put('/users/:index',checkUserInArray,(req,res) => {
   const { index } = req.params;
   const { name }  = req.body;
   console.log(req.body)
@@ -36,7 +55,7 @@ server.put('/users/:index', (req,res) => {
   return res.json(users)
 })
 
-server.delete('/users/:index', (req,res) => {
+server.delete('/users/:index',checkUserInArray, (req,res) => {
   const { index } = req.params;
   users.splice(index , 1);
 
