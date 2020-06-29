b7a934b1f350e3a7390dfb5b19c97abc85c86977
cristiano <crisrfab@gmail.com>
2020-06-28 21:40:33 -0300

    finish graphql server

--- Free-Code-Camp/Api and Microservices/MongoDB/myApp.js
+++ Free-Code-Camp/Api and Microservices/MongoDB/myApp.js
@@ -7,14 +7,13 @@
 /*  ================== */
 
 /** 1) Install & Set up mongoose */
-
 // Add mongodb and mongoose to the project's package.json. Then require 
 // mongoose. Store your Mongo Atlas database URI in the private .env file 
 // as MONGO_URI. Connect to the database using the following syntax:
-//
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true }); 
-
-
+require('dotenv/config');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
