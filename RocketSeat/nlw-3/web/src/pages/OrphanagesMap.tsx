33536d2da8f3c93e87c14fd6a2ef2759c71ac0cb
Cristiano Rocha <crisrfab@gmail.com>
2020-10-12 16:30:36 -0300

    nlw-3 day 1

--- RocketSeat/nlw-3/web/src/pages/OrphanagesMap.tsx
+++ RocketSeat/nlw-3/web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
+
 import mapMarkerImg from '../images/map-marker.svg'
 import '../styles/pages/orphanages-map.css'
 function OrphanagesMap() {
@@ -19,6 +23,18 @@ function OrphanagesMap() {
                     <span>São Paulo</span>
                 </footer>
             </aside>
+
+            <Map 
+                center={[-23.5815452,-46.5270243]}
+                zoom={15}
+                style={ { width:'100%',height: '100%' } }
+            >
+
+                {/* <TileLayer url={`https://a.tile.openstreetmap.org/{z}/{x}/{y}.png`}/> */}
+
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+            </Map>
+
             <Link to="" className="create-orphanage">
                     <FiPlus size={32} color="#FFF" />
             </Link>
