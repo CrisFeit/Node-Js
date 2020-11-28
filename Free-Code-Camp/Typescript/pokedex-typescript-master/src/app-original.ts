778d22ee8ada880d514f0bf9905a26292e336fe8
Cristiano Rocha <crisrfab@gmail.com>
2020-11-28 01:59:49 -0300

    finish pokedex typescript

---
+++ Free-Code-Camp/Typescript/pokedex-typescript-master/src/app-original.ts
@@ -0,0 +1,46 @@
+const container: HTMLElement | any = document.getElementById('app')
+const pokemons: number = 20
+
+interface IPokemon {
+    id: number,
+    name: string,
+    image: string,
+    type: string
+}
+
+function fetchData(): void {
+    for (let i = 1; i <= pokemons; i++) {
+        getPokemon(i)
+    }
+}
+
+async function getPokemon(id: number): Promise<void> {
+    const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const pokemon: any = await data.json()
+    const pokemonType: string = pokemon.types
+        .map((poke: any) => poke.type.name)
+        .join(', ')
+
+    const transformedPokemon = {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: `${pokemon.sprites.front_default}`,
+        type: pokemonType,
+    }
+
+    showPokemon(transformedPokemon)
+}
+
+function showPokemon(pokemon: IPokemon): void {
+    let output: string = `
+          <div class="card">
+              <span class="card__id">#${pokemon.id}</span>
+              <img class="card__image" src=${pokemon.image} alt=${pokemon.name} />
+              <h1 class="card__name">${pokemon.name}</h1>
+              <span class="card__details">${pokemon.type}</span>
+          </div>
+      `
+    container.innerHTML += output
+}
+
+fetchData()
