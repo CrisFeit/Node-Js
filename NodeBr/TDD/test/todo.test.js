5b6476161ddef1a78ac65869e1806e38474926f1
Cristiano Rocha <crisrfab@gmail.com>
2021-01-23 14:59:48 -0300

    create tdd env

---
+++ NodeBr/TDD/test/todo.test.js
@@ -0,0 +1,17 @@
+const { describe, it, before } = require('mocha')
+const { expect,  } = require('chai')
+const { } = require('sinon')
+
+describe('todo' , ()=> {
+    describe("#isValid" , ()=> {
+        it('should return invalid when creating an object without text' , ()=> {
+            
+        })
+        it('should return invalid when creating an object using the "when" property invalid' , ()=> {
+            
+        })
+        it('should have "id", "text", "when" and "status" properties after creating object ' , ()=> {
+            
+        })
+    })
+})
